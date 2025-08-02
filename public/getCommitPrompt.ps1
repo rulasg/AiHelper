7f94aca222261d335493025f65f3be80a3e3c72a
rulasg <rulasg@github.com>
2025-08-02 23:48:58 +0200

    feat(getCommitPrompt): add alias, customize prompts, and improve instructions handling

--- public/getCommitPrompt.ps1
+++ public/getCommitPrompt.ps1
@@ -1,23 +1,49 @@
 function Get-AiMessageForCommit{
     [CmdletBinding()]
+    [alias("aicm")]
     param(
-        [Parameter()]$Model = "openai/gpt-4o-mini"
+        [Parameter()]$Model = "openai/gpt-4.1",
+        [Parameter()]$Prompt
     )
 
     $gitdifff = git diff --staged
 
+    if ([string]::IsNullOrWhiteSpace($gitdifff)) {
+        Write-Host "No staged changes found. Please stage your changes before generating a commit message." -ForegroundColor Yellow
+        return $null
+    }
+
     $instructions = Get-Instructions -Type CommitMessage
 
-    $prompt = @()
-    $prompt += "Propose a git commit message."
-    $prompt += "Output just the message."
-    $prompt += "Make the message a single line."
-    $prompt += "Staged changes: $gitdifff"
-    $prompt += "Follow these instructions: $instructions"
+    if ($null -eq $instructions) {
+        Write-Verbose "No instructions found at .github/$instFileName. Using default commit message format."
+    } else {
+        Write-Verbose "Instructions: .github [$($instructions.Length)] characters."
+    }
 
+    $p = @()
+    $p += "Follow these instructions: [ $instructions ]"
+    $sysPrompt = $p -join "`n"
     
-    gh models run $Model $prompt
-} Export-ModuleMember -Function Get-AiMessageForCommit
+    $p = @()
+    $p += "Propose a git commit message."
+    $p += "Output just the message."
+    $p += "Make the message a single line."
+    $p += "Result of git diff --staged: [ $gitdifff ]"
+    $p += "User description of the changes: [ $Prompt ]"
+    $usrprompt = $p -join "`n"
+
+    Write-Verbose "Module: $Model"
+    Write-Verbose "SysPrompt: $sysPrompt"
+    Write-Verbose "UsrPrompt: $usrprompt"
+
+    $message = gh models run $Model "$usrprompt" --system-prompt $sysPrompt
+
+    $global:message = $message | Out-String
+
+    return $message
+
+} Export-ModuleMember -Function Get-AiMessageForCommit -Alias aicm
 
 function Get-Instructions{
     [CmdletBinding()]
